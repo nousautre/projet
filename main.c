d0b067db012238cbd6ac15077e803636220db958
casanogab <casanogab@hotmail.com>
2014-05-16 11:12:12 -0400

    Ajout dune fonction qui lis une ligne de caractere malheureusement des quun espace est croise la ligne ne lis plus.... je vais ajuster ca

--- main.c
+++ main.c
@@ -10,12 +10,29 @@ void nombrePointTotal(int * nombreDePointTotal){
  scanf("%d", &*nombreDePointTotal);
 }
 
+// cette procédure ( qui ne renvoie rien) lis la coordonée actuelle
+// verifier si lutilisateur entre plein despace consécutive
+
+void coordoneePointXYActuel(char * ligneLuCoordonee){
+ scanf("%127s", &*ligneLuCoordonee);
+}
+
+//void coordoneePointXYActuel(int nombreDePointTotal, int * tableauCoordoneePointXY){
+
+
 
 // La fonction main ( retourne 0 lorsque bien complété)
 // lis le nombre de point total au clavier
 int main(){
     int nombreDePointTotal=0;
+    char ligneLuCoordonee[128];
+    printf( "BIENVENUE \n"); // SERA RETIRER
+    
     nombrePointTotal(&nombreDePointTotal);
-printf( "BIENVENUE %i\n", nombreDePointTotal); //
+    printf( "Total de point: %i\n", nombreDePointTotal); // SERA RETIRER
+    coordoneePointXYActuel(&ligneLuCoordonee);
+    printf( "La ligne lu: %s\n", ligneLuCoordonee); // SERA RETIRER
+
+
 return 0;
 }
