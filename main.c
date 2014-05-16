853f3eaf416ea468c1e819d6809db604d324459b
casanogab <casanogab@hotmail.com>
2014-05-16 11:19:48 -0400

    Modification de la fonction pour accepter le point X et Y en lecture

--- main.c
+++ main.c
@@ -13,8 +13,9 @@ void nombrePointTotal(int * nombreDePointTotal){
 // cette procédure ( qui ne renvoie rien) lis la coordonée actuelle
 // verifier si lutilisateur entre plein despace consécutive
 
-void coordoneePointXYActuel(char * ligneLuCoordonee){
- scanf("%127s", &*ligneLuCoordonee);
+void coordoneePointXYActuel(double * pointXActuel, double * pointYActuel){
+ scanf("%lf", &*pointXActuel);
+ scanf("%lf", &*pointYActuel);
 }
 
 //void coordoneePointXYActuel(int nombreDePointTotal, int * tableauCoordoneePointXY){
@@ -23,16 +24,17 @@ void coordoneePointXYActuel(char * ligneLuCoordonee){
 
 // La fonction main ( retourne 0 lorsque bien complété)
 // lis le nombre de point total au clavier
+// lis la première coordonée entre
 int main(){
     int nombreDePointTotal=0;
-    char ligneLuCoordonee[128];
+    double pointXActuel=0;
+    double pointYActuel=0;
     printf( "BIENVENUE \n"); // SERA RETIRER
-    
     nombrePointTotal(&nombreDePointTotal);
     printf( "Total de point: %i\n", nombreDePointTotal); // SERA RETIRER
-    coordoneePointXYActuel(&ligneLuCoordonee);
-    printf( "La ligne lu: %s\n", ligneLuCoordonee); // SERA RETIRER
-
+    coordoneePointXYActuel(&pointXActuel,&pointYActuel);
+    printf( "Coordone X: %lf\n", pointXActuel); // SERA RETIRER
+    printf( "Coordone Y: %lf\n", pointYActuel); // SERA RETIRER
 
 return 0;
 }
