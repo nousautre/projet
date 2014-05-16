9dc3ec1a726ea2c690c6a8e8fa637308692163ea
casanogab <casanogab@hotmail.com>
2014-05-16 12:01:23 -0400

    ajout de la boucle for qui call le bon nombre de fois la procédure de lecture de chiffre et ajoute dans un tableau temporaire les chiffres lu

--- main.c
+++ main.c
@@ -6,8 +6,8 @@
 #include <stdio.h>
 
 // cette procédure ( qui ne renvoie rien) lis le nombre de point total attendu
-void nombrePointTotal(int * nombreDePointTotal){
- scanf("%d", &*nombreDePointTotal);
+void nombrePointTotal(int * NOMBREDEPOINTTOTAL){
+ scanf("%d", &*NOMBREDEPOINTTOTAL);
 }
 
 // cette procédure ( qui ne renvoie rien) lis la coordonée actuelle
@@ -16,14 +16,20 @@ void nombrePointTotal(int * nombreDePointTotal){
 void coordoneePointXYActuel(double * pointXActuel, double * pointYActuel){
  scanf("%lf", &*pointXActuel);
  scanf("%lf", &*pointYActuel);
+ printf( "Coordone X: %lf\n", *pointXActuel); // SERA RETIRER
+ printf( "Coordone Y: %lf\n", *pointYActuel); // SERA RETIRER
 }
 
-void construitTableauCoordoneePointXY(int nombreDePointTotal, int * tableauCoordoneePointXY){
+void construitTableauCoordoneePointXY(const int NOMBREDEPOINTTOTAL, int * tableauCoordoneePointXY){
     double pointXActuel=0;
     double pointYActuel=0;
-    coordoneePointXYActuel(&pointXActuel,&pointYActuel);
-    printf( "Coordone X: %lf\n", pointXActuel); // SERA RETIRER
-    printf( "Coordone Y: %lf\n", pointYActuel); // SERA RETIRER
+    int i=1;
+    for (i;i<=NOMBREDEPOINTTOTAL;++i){
+        coordoneePointXYActuel(&pointXActuel,&pointYActuel);
+       tableauCoordoneePointXY[i] =  pointXActuel; // pour l'instant ce n'est pas un tableau de structure alors je ne garde que le x
+    }
+    
+    
 }
 
 
@@ -31,12 +37,12 @@ void construitTableauCoordoneePointXY(int nombreDePointTotal, int * tableauCoord
 // lis le nombre de point total au clavier
 // lis la première coordonée entre
 int main(){
-    const unsigned int NOMBREDEPOINTTOTAL;
-    
+    const unsigned int NOMBREDEPOINTTOTAL; // a verifier aupres de l'enseignant si cest valide de declarer et dassigner apres la valeur de la constante....
     printf( "BIENVENUE \n"); // SERA RETIRER
     nombrePointTotal(&NOMBREDEPOINTTOTAL);
+    int tableauCoordoneePointXY[NOMBREDEPOINTTOTAL]; // combien de point total par defaut en assignant une valeur non dynamique?? création de tableau non dynamique
     printf( "Total de point: %i\n", NOMBREDEPOINTTOTAL); // SERA RETIRER
-    
+    construitTableauCoordoneePointXY(NOMBREDEPOINTTOTAL,&tableauCoordoneePointXY);
 
 return 0;
 }
