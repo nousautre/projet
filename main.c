6cb8b5fc71c4f8ffd7100985a2536d3b04d03af2
casanogab <casanogab@hotmail.com>
2014-05-16 11:26:43 -0400

    Creation dun procédure séparer Constante pour point total

--- main.c
+++ main.c
@@ -18,23 +18,25 @@ void coordoneePointXYActuel(double * pointXActuel, double * pointYActuel){
  scanf("%lf", &*pointYActuel);
 }
 
-//void coordoneePointXYActuel(int nombreDePointTotal, int * tableauCoordoneePointXY){
-
+void construitTableauCoordoneePointXY(int nombreDePointTotal, int * tableauCoordoneePointXY){
+    double pointXActuel=0;
+    double pointYActuel=0;
+    coordoneePointXYActuel(&pointXActuel,&pointYActuel);
+    printf( "Coordone X: %lf\n", pointXActuel); // SERA RETIRER
+    printf( "Coordone Y: %lf\n", pointYActuel); // SERA RETIRER
+}
 
 
 // La fonction main ( retourne 0 lorsque bien complété)
 // lis le nombre de point total au clavier
 // lis la première coordonée entre
 int main(){
-    int nombreDePointTotal=0;
-    double pointXActuel=0;
-    double pointYActuel=0;
+    const unsigned int NOMBREDEPOINTTOTAL;
+    
     printf( "BIENVENUE \n"); // SERA RETIRER
-    nombrePointTotal(&nombreDePointTotal);
-    printf( "Total de point: %i\n", nombreDePointTotal); // SERA RETIRER
-    coordoneePointXYActuel(&pointXActuel,&pointYActuel);
-    printf( "Coordone X: %lf\n", pointXActuel); // SERA RETIRER
-    printf( "Coordone Y: %lf\n", pointYActuel); // SERA RETIRER
+    nombrePointTotal(&NOMBREDEPOINTTOTAL);
+    printf( "Total de point: %i\n", NOMBREDEPOINTTOTAL); // SERA RETIRER
+    
 
 return 0;
 }
