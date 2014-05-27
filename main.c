39e19955521b8e3762e43b346d3eb30c4d70c0a5
casanogab <casanogab@hotmail.com>
2014-05-27 14:02:47 -0400

    ajout des arctan fonction

--- main.c
+++ main.c
@@ -4,11 +4,12 @@
 
 
 #include <stdio.h>
+#include <math.h>
 
 
 typedef struct {
-                double x;
-                double y;
+                double ptX;
+                double ptY;
  }coordonee;
 
 
@@ -35,8 +36,8 @@ void construitTableauCoordoneePointXY(const int NOMBREDEPOINTTOTAL, coordonee ta
     int i=0;
     for (i;i<NOMBREDEPOINTTOTAL;++i){
        coordoneePointXYActuel(&pointXActuel,&pointYActuel);
-       tableauCoordoneePointXY[i].x = pointXActuel; // pour l'instant ce n'est pas un tableau de structure alors je ne garde que le x
-       tableauCoordoneePointXY[i].y = pointYActuel;
+       tableauCoordoneePointXY[i].ptX = pointXActuel; // pour l'instant ce n'est pas un tableau de structure alors je ne garde que le x
+       tableauCoordoneePointXY[i].ptY = pointYActuel;
     }   
 }
 
@@ -44,8 +45,8 @@ void construitTableauCoordoneePointXY(const int NOMBREDEPOINTTOTAL, coordonee ta
 void afficheTableauCoordoneePointXY(const int NOMBREDEPOINTTOTAL, coordonee tableauCoordoneePointXY[]){
     int i = 0;
     for (i;i<NOMBREDEPOINTTOTAL;++i){
-         printf( "Tableau espace  le X vaut: %lf \n",tableauCoordoneePointXY[i].x);  
-         printf( "Tableau espace  le Y vaut: %lf \n",tableauCoordoneePointXY[i].y);
+         printf( "Tableau espace  le X vaut: %lf \n",tableauCoordoneePointXY[i].ptX);  
+         printf( "Tableau espace  le Y vaut: %lf \n",tableauCoordoneePointXY[i].ptY);
     }   
 }
 
@@ -53,7 +54,7 @@ void afficheTableauCoordoneePointXY(const int NOMBREDEPOINTTOTAL, coordonee tabl
 // y du pointA - y du pointB / x du pointA - x du pointB 
 // tout ca dans l'absolu
 double calculDelaPente(coordonee pointA, coordonee pointB){
-        return (pointA.y-pointB.y)/(pointA.x-pointB.x);
+        return (pointA.ptY-pointB.ptY)/(pointA.ptX-pointB.ptX);
 }
 
 // Construit le tableau de pente de chacun des points
@@ -69,21 +70,43 @@ int i = 0;
 void afficheTableauDePente(const int NOMBREDEPOINTTOTAL,double tableauDePente[]){
     int i = 0;
     for (i;i<NOMBREDEPOINTTOTAL-1;++i){
-         printf( "Tableau espace la pente vaut: %lf \n",tableauDePente[i]);  
+         printf( "\nTableau espace la pente vaut: %lf \n",tableauDePente[i]);  
     }   
+}
 
+// cette procédure calcule et renvoie artan d'un angle
+double calculDeLArcTan(double pente){
+        return atan(pente);
+}
+
+// Construit le tableau de pente de chacun des points
+// Le calcul s'effectue sur NOMBREDEPOINTTOTAL -1 car le dernier point n'a pas de suivant
+void construitTableauDeLArcTan(const int NOMBREDEPOINTTOTAL, double tableauDePente[], double tableauDeArcTan[]){
+int i = 0;
+    for (i;i<NOMBREDEPOINTTOTAL-1;++i){
+         tableauDeArcTan[i] = calculDeLArcTan(tableauDePente[i]);
+    }
+}
+
+//cette procedure affiche le tableau de pente
+void afficheTableauDeLArcTan(const int NOMBREDEPOINTTOTAL,double tableauDeArcTan[]){
+    int i = 0;
+    for (i;i<NOMBREDEPOINTTOTAL-1;++i){
+         printf( "\nARCTAN vaut: %lf \n",tableauDeArcTan[i]);  
+    }   
 }
 
 // La fonction main ( retourne 0 lorsque bien complété)
 // lis le nombre de point total au clavier
 // lis la première coordonée entre
 int main(int argc, char ** argv){
-    const unsigned int NOMBREDEPOINTTOTAL; // a verifier aupres de l'enseignant si cest valide de declarer et dassigner apres la valeur de la constante....
+    int NOMBREDEPOINTTOTAL; // a verifier aupres de l'enseignant si cest valide de declarer et dassigner apres la valeur de la constante....
     printf( "BIENVENUE \n"); // SERA RETIRER
     nombrePointTotal(&NOMBREDEPOINTTOTAL);
     
     coordonee tableauCoordoneePointXY[NOMBREDEPOINTTOTAL]; // combien de point total par defaut en assignant une valeur non dynamique?? création de tableau non dynamique
     double tableauDePente[NOMBREDEPOINTTOTAL-1];
+    double tableauDeArcTan[NOMBREDEPOINTTOTAL-1];
     
     printf( "\nTotal de point: %i\n", NOMBREDEPOINTTOTAL); // SERA RETIRER
     
@@ -92,6 +115,9 @@ int main(int argc, char ** argv){
 
     construitTableauDePente(NOMBREDEPOINTTOTAL,tableauCoordoneePointXY,tableauDePente);// un tableau cest toujours passe en référence je crois
     afficheTableauDePente(NOMBREDEPOINTTOTAL,tableauDePente);
+    
+    construitTableauDeLArcTan(NOMBREDEPOINTTOTAL,tableauCoordoneePointXY,tableauDePente);// un tableau cest toujours passe en référence je crois
+    afficheTableauDeLArcTan(NOMBREDEPOINTTOTAL,tableauDePente);
 return 0;
 }
 
